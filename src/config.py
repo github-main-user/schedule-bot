2df25b6ea7291870088b3a1a4121ff7507d33d63
github-main-user <kolya.skripn@mail.ru>
2025-01-14 20:56:59 +0300

    add config file

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import pytz
+from datetime import time
+
+MSK_TZ = pytz.timezone('Etc/GMT-3')
+SCHEDULE_UPDATE_TIME = time(22, 0, tzinfo=MSK_TZ)
+
+DAILY_TEMPLATE = """\
+{minutes_left} minutes left before the next lecture:'
+*{lecture_time}*: {lecture_name} ({cabinet})'
+{teacher} ({is_practice})"""
