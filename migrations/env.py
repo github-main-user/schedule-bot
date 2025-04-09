1b9a4da0bccae4e2e204d1d913b4e26ad0450032
github-main-user <kolya.skripn@mail.ru>
2025-04-09 19:45:25 +0300

    fix imports

--- migrations/env.py
+++ migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import engine_from_config, pool
 
 from src.config import settings
 from src.db import Base
-from src.models import *  # noqa
+from src.models.schedule import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
