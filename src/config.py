d23436720c25888288b410b89d55264bdc3e23de
github-main-user <kolya.skripn@mail.ru>
2025-04-09 15:19:32 +0300

    update config

--- src/config.py
+++ src/config.py
@@ -1,48 +1,61 @@
-import math
-from datetime import date, datetime, time
-from parser import Lecture
+from datetime import time
 
 import pytz
-
-MSK_TZ = pytz.timezone('Etc/GMT-3')
-SCHEDULE_UPDATE_TIME = time(22, 0, tzinfo=MSK_TZ)
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-def format_next_lecture(lecture: Lecture, date: date) -> str:
-    minutes_left = math.ceil(
-        (
-            datetime.combine(date, lecture.time, tzinfo=MSK_TZ)
-            - datetime.now(tz=MSK_TZ)
-        ).total_seconds()
-        / 60
-    )
-    is_practice = 'практика' if lecture.discipline.is_practice else 'лекция'
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_NAME: str
+    DB_PASS: str
+    TELEGRAM_TOKEN: str
+    GROUP_ID: int
 
-    return (
-        f'{minutes_left} minutes left before the next lecture:\n'
-        f'*{lecture.time.strftime('%H:%M')}*: {lecture.discipline.name} ({lecture.cabinet})\n'
-        f'{lecture.teacher} ({is_practice})'
-    )
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
+    @property
+    def SCHEDULE_URL(self) -> str:
+        return f"https://sksi.ru/Students/ScheduleJson/?id={self.GROUP_ID}"
 
-def format_lectures(lectures: list[Lecture], date: date) -> str:
-    formatted_lectures = [date.strftime('*%d %b (%a)*')]
-    for lecture in sorted(lectures, key=lambda l: l.time):
-        formatted_lectures.append(
-            f'*{lecture.time.strftime('%H:%M')}*: {lecture.discipline.name} ({lecture.cabinet})'
-        )
+    TZ: pytz.BaseTzInfo = pytz.timezone("Etc/GMT-3")
+    SCHEDULE_UPDATE_TIME: time = time(hour=22, minute=0, tzinfo=TZ)
 
-    return '\n'.join(formatted_lectures)
+    model_config = SettingsConfigDict(env_file=".env")
 
 
-def format_jobs(jobs) -> str:
-    formatted_jobs = ['Scheduled jobs:\n']
-    for job in jobs:
-        formatted_jobs.append(
-            f'Job "{job.name}":\n'
-            f' - id: {job.id}\n'
-            f' - callback: {job.callback}\n'
-            f' - trigger: {job.trigger}\n'
-        )
+settings = Settings()  # type: ignore
 
-    return '\n'.join(formatted_jobs)
+#
+# def format_next_lecture(lecture: Lecture, date: date) -> str:
+#     minutes_left = math.ceil(
+#         (datetime.combine(date, lecture.time, tzinfo=MSK_TZ) - datetime.now(tz=MSK_TZ)).total_seconds() / 60
+#     )
+#     is_practice = "практика" if lecture.discipline.is_practice else "лекция"
+#
+#     return (
+#         f"{minutes_left} minutes left before the next lecture:\n"
+#         f"*{lecture.time.strftime('%H:%M')}*: {lecture.discipline.name} ({lecture.cabinet})\n"
+#         f"{lecture.teacher} ({is_practice})"
+#     )
+#
+#
+# def format_lectures(lectures: list[Lecture], date: date) -> str:
+#     formatted_lectures = [date.strftime("*%d %b (%a)*")]
+#     for lecture in sorted(lectures, key=lambda l: l.time):
+#         formatted_lectures.append(f"*{lecture.time.strftime('%H:%M')}*: {lecture.discipline.name} ({lecture.cabinet})")
+#
+#     return "\n".join(formatted_lectures)
+#
+#
+# def format_jobs(jobs) -> str:
+#     formatted_jobs = ["Scheduled jobs:\n"]
+#     for job in jobs:
+#         formatted_jobs.append(
+#             f'Job "{job.name}":\n' f" - id: {job.id}\n" f" - callback: {job.callback}\n" f" - trigger: {job.trigger}\n"
+#         )
+#
+#     return "\n".join(formatted_jobs)
