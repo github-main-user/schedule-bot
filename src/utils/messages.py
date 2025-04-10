94bfd98c6456019fea705ae7c9568ff55f73bd83
github-main-user <kolya.skripn@mail.ru>
2025-04-10 18:09:13 +0300

    add update subscribe messages

--- src/utils/messages.py
+++ src/utils/messages.py
@@ -1,2 +1,4 @@
 SUBSCRIBED = "You were subscribed to receive updates in schedule every day at {time}."
 UNSUBSCRIBED = "You are unsubscribed now."
+ALREADY_SUBSCRIBED = "You are already subscribed."
+NOT_SUBSCRIBED = "You are not subscribed."
