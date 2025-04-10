573486050f3e71c96e4d6686deee02a1908b3aa4
github-main-user <kolya.skripn@mail.ru>
2025-04-10 18:23:59 +0300

    add connect handlers to repo

--- src/handlers.py
+++ src/handlers.py
@@ -2,32 +2,46 @@ from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
 
 from src.config import settings
+from src.db import get_session
+from src.repositories.subscriber_repository import SubscriberRepository
 from src.utils import messages
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.effective_chat is None:
         return
-
     chat_id = update.effective_chat.id
 
-    subscribe_msg = messages.SUBSCRIBE.format(time=settings.SCHEDULE_UPDATE_TIME.strftime("%H:%M"))
+    async with await get_session() as session:
+        repo = SubscriberRepository(session)
+
+        if await repo.exists(chat_id):
+            message = messages.ALREADY_SUBSCRIBED
+        else:
+            await repo.create(chat_id)
+            message = messages.SUBSCRIBED.format(time=settings.SCHEDULE_UPDATE_TIME.strftime("%H:%M"))
+
     await context.bot.send_message(
         chat_id=chat_id,
-        text=subscribe_msg,
+        text=message,
     )
 
 
 async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.effective_chat is None:
         return
-
     chat_id = update.effective_chat.id
 
-    await context.bot.send_message(
-        chat_id=chat_id,
-        text=messages.UNSUBSCRIBE,
-    )
+    async with await get_session() as session:
+        repo = SubscriberRepository(session)
+
+        if await repo.exists(chat_id):
+            await repo.delete(chat_id)
+            message = messages.UNSUBSCRIBED
+        else:
+            message = messages.NOT_SUBSCRIBED
+
+    await context.bot.send_message(chat_id=chat_id, text=message)
 
 
 command_handlers = [
