112f7311fc4b24327ff9a24170f8ecfb60643518
github-main-user <kolya.skripn@mail.ru>
2025-04-10 18:16:28 +0300

    add new methods

--- src/repositories/subscriber_repository.py
+++ src/repositories/subscriber_repository.py
@@ -10,13 +10,22 @@ class SubscriberRepository:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    async def create(self, subscriber: Subscriber) -> None:
-        self.session.add(subscriber)
+    async def create(self, chat_id: int) -> None:
+        new_subscriber = Subscriber(chat_id=chat_id)
+        self.session.add(new_subscriber)
         await self.session.commit()
 
-    async def delete(self, subscriber: Subscriber) -> None:
-        await self.session.delete(subscriber)
-        await self.session.commit()
+    async def delete(self, chat_id: int) -> None:
+        result = await self.get_by_chat_id(chat_id)
+        await self.session.delete(result)
+
+    async def exists(self, chat_id: int) -> bool:
+        return bool(await self.get_by_chat_id(chat_id))
+
+    async def get_by_chat_id(self, chat_id: int) -> Subscriber | None:
+        stmt = select(Subscriber).filter_by(chat_id=chat_id)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
 
     async def get_all(self) -> Sequence[Subscriber]:
         stmt = select(Subscriber)
