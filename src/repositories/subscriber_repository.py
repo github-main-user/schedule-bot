ec394c906f4c34e3c321cd6b8d7de52530f4517b
github-main-user <kolya.skripn@mail.ru>
2025-04-10 17:28:38 +0300

    make repository for subscriber model

---
+++ src/repositories/subscriber_repository.py
@@ -0,0 +1,20 @@
+from typing import Sequence
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.models.subscribers import Subscriber
+
+
+class SubscriberRepository:
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    async def create(self, subscriber: Subscriber) -> None:
+        self.session.add(subscriber)
+        await self.session.commit()
+
+    async def get_all(self) -> Sequence[Subscriber]:
+        stmt = select(Subscriber)
+        result = await self.session.execute(stmt)
+        return result.scalars().all()
