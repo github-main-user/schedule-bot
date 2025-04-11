b80bc428565796887d9878dfdb684598faeae348
github-main-user <kolya.skripn@mail.ru>
2025-04-11 19:16:10 +0300

    remove old commented code

--- src/config.py
+++ src/config.py
@@ -38,34 +38,3 @@ class Settings(BaseSettings):
 
 
 settings = Settings()  # type: ignore
-
-#
-# def format_next_lecture(lecture: Lecture, date: date) -> str:
-#     minutes_left = math.ceil(
-#         (datetime.combine(date, lecture.time, tzinfo=MSK_TZ) - datetime.now(tz=MSK_TZ)).total_seconds() / 60
-#     )
-#     is_practice = "практика" if lecture.discipline.is_practice else "лекция"
-#
-#     return (
-#         f"{minutes_left} minutes left before the next lecture:\n"
-#         f"*{lecture.time.strftime('%H:%M')}*: {lecture.discipline.name} ({lecture.cabinet})\n"
-#         f"{lecture.teacher} ({is_practice})"
-#     )
-#
-#
-# def format_lectures(lectures: list[Lecture], date: date) -> str:
-#     formatted_lectures = [date.strftime("*%d %b (%a)*")]
-#     for lecture in sorted(lectures, key=lambda l: l.time):
-#         formatted_lectures.append(f"*{lecture.time.strftime('%H:%M')}*: {lecture.discipline.name} ({lecture.cabinet})")
-#
-#     return "\n".join(formatted_lectures)
-#
-#
-# def format_jobs(jobs) -> str:
-#     formatted_jobs = ["Scheduled jobs:\n"]
-#     for job in jobs:
-#         formatted_jobs.append(
-#             f'Job "{job.name}":\n' f" - id: {job.id}\n" f" - callback: {job.callback}\n" f" - trigger: {job.trigger}\n"
-#         )
-#
-#     return "\n".join(formatted_jobs)
