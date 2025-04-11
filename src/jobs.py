ba9afc415469d9334374c8a45b9608c210e18979
github-main-user <kolya.skripn@mail.ru>
2025-04-11 20:04:23 +0300

    add daily_update job

--- src/jobs.py
+++ src/jobs.py
@@ -1,69 +1,32 @@
-from datetime import datetime, timedelta
-from parser import ScheduleFormer
+from datetime import datetime
 
 from telegram.ext import ContextTypes
 
-from config import MSK_TZ, format_lectures, format_next_lecture
-
-
-async def send_lecture_info_job(context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send inforemation about a specific lecture."""
-    chat_id, date, lecture = context.job.data  # type: ignore
-
-    await context.bot.send_message(
-        chat_id=chat_id,  # type: ignore
-        text=format_next_lecture(lecture, date),
-        parse_mode='Markdown',
-    )
+from src.config import settings
+from src.db import get_session
+from src.repositories.schedule_repository import ScheduleRepository
+from src.repositories.subscriber_repository import SubscriberRepository
+from src.utils import messages
 
 
 async def daily_check(context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Perform the daily check to update the schedule."""
-    chat_id = context.job.data  # type: ignore
+    session = await get_session()
+    schedule_repo = ScheduleRepository(session)
+    subscriber_repo = SubscriberRepository(session)
 
-    try:
-        # Generate the schedule
-        schedule = ScheduleFormer().form_schedule()
-        tomorrow_date = (datetime.now(tz=MSK_TZ) + timedelta(days=1)).date()
-        tomorrow_lectures = schedule.get(tomorrow_date, [])
+    # TODO: fix now to now + 1 day
+    tomorrow_lectures = await schedule_repo.get_lectures_for_day(datetime.now(settings.TZ).date())
 
-        if tomorrow_lectures:
-            await context.bot.send_message(
-                chat_id=chat_id,  # type: ignore
-                text=f"Tomorrow will be *{len(tomorrow_lectures)}* lecture(s):",
-                parse_mode='Markdown',
-            )
+    if tomorrow_lectures:
+        message = messages.TOMORROW_N_LECTURES.format(n=len(tomorrow_lectures))
+    else:
+        message = messages.EMPTY_TOMORROW
 
-            # Schedule reminders for lectures
-            for lecture in tomorrow_lectures:
-                context.job_queue.run_once(  # type: ignore
-                    send_lecture_info_job,
-                    when=datetime.combine(tomorrow_date, lecture.time, tzinfo=MSK_TZ)
-                    - timedelta(minutes=15),
-                    data=(chat_id, tomorrow_date, lecture),
-                    name=lecture.discipline.name,
-                )
+    subscribers = await subscriber_repo.get_all()
+    print(subscribers)
 
-            await context.bot.send_message(
-                chat_id=chat_id,  # type: ignore
-                text=format_lectures(tomorrow_lectures, tomorrow_date),
-                parse_mode='Markdown',
-            )
-        else:
-            # Notify about the next lecture day
-            nearest_day = min(schedule.keys()) if schedule else None
-            if nearest_day:
-                await context.bot.send_message(
-                    chat_id=chat_id,  # type: ignore
-                    text=f"The nearest lecture will be on {nearest_day.strftime('%d %b, %a')} ({(nearest_day - datetime.now().date()).days} day(s)).",
-                )
-            else:
-                await context.bot.send_message(
-                    chat_id=chat_id,  # type: ignore
-                    text="There are no upcoming lectures.",
-                )
-    except Exception as e:
+    for subscriber in subscribers:
         await context.bot.send_message(
-            chat_id=chat_id,  # type: ignore
-            text="An error occurred while updating the schedule.",
+            chat_id=subscriber.chat_id,
+            text=message,
         )
