999cb00c72741bfb9e40f9063232ec6c194e9f96
github-main-user <kolya.skripn@mail.ru>
2025-04-11 19:48:51 +0300

    add TOMORROW messages

--- src/utils/messages.py
+++ src/utils/messages.py
@@ -2,3 +2,6 @@ SUBSCRIBED = "You were subscribed to receive updates in schedule every day at {t
 UNSUBSCRIBED = "You are unsubscribed now."
 ALREADY_SUBSCRIBED = "You are already subscribed."
 NOT_SUBSCRIBED = "You are not subscribed."
+
+EMPTY_TOMORROW = "There are no lectures tomorrow."
+TOMORROW_N_LECTURES = "Tomorrow will me {n} lectures"
