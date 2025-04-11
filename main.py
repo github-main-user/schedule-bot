97b1fc11f1370328f995fa2cf8f8839a04224782
github-main-user <kolya.skripn@mail.ru>
2025-04-11 20:04:00 +0300

    start jobs when program starts

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from telegram.ext import ApplicationBuilder
 
 from src.config import settings
 from src.handlers.subscribers import command_handlers
+from src.jobs import daily_check
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
@@ -13,6 +14,14 @@ def main() -> None:
     app = ApplicationBuilder().token(settings.TELEGRAM_TOKEN).build()
 
     app.add_handlers(command_handlers)
+
+    job_queue = app.job_queue
+    if not job_queue:
+        logging.error("job queue is None")
+        return
+
+    job_queue.run_once(daily_check, 0)
+
     app.run_polling()
 
 
