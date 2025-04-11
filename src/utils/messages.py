f5ae5e1ee71779798e3e534458a517b5e3626401
github-main-user <kolya.skripn@mail.ru>
2025-04-11 22:00:10 +0300

    refactor messages

--- src/utils/messages.py
+++ src/utils/messages.py
@@ -1,7 +1,7 @@
-SUBSCRIBED = "You were subscribed to receive updates in schedule every day at {time}."
-UNSUBSCRIBED = "You are unsubscribed now."
-ALREADY_SUBSCRIBED = "You are already subscribed."
-NOT_SUBSCRIBED = "You are not subscribed."
+SUBSCRIBED = "You were subscribed to receive updates in schedule every day at {time}"
+UNSUBSCRIBED = "You are unsubscribed now"
+ALREADY_SUBSCRIBED = "You are already subscribed"
+NOT_SUBSCRIBED = "You are not subscribed"
 
-EMPTY_TOMORROW = "There are no lectures tomorrow."
-TOMORROW_N_LECTURES = "Tomorrow will me {n} lectures"
+EMPTY_TOMORROW = "There are no lectures tomorrow"
+TOMORROW_N_LECTURES = "Tomorrow will be {n} lecture(s)"
