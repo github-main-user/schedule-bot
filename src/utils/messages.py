39083ec94861e913bf6aee6161d4fce208ddacbc
github-main-user <kolya.skripn@mail.ru>
2025-04-12 17:55:20 +0300

    feat: add help message

--- src/utils/messages.py
+++ src/utils/messages.py
@@ -1,3 +1,14 @@
+HELP = """\
+It it a bot for the SKSI institute.
+Bot fetches and shows schedule for the "ИТБ-24" group.
+Bot will update schedule every day a {time}, and notify you about every lecture at {n} minutes before it starts.
+
+Available commands:
+- /help - show this help message
+- /start - start bot
+- /stop - stop bot
+"""
+
 SUBSCRIBED = "You were subscribed to receive updates in schedule every day at {time}"
 UNSUBSCRIBED = "You are unsubscribed now"
 ALREADY_SUBSCRIBED = "You are already subscribed"
