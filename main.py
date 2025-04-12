c2784fb13a5037ef7c72a00869ac1f2b88fc927c
github-main-user <kolya.skripn@mail.ru>
2025-04-12 15:18:30 +0300

    feat: extract jobs setup to separate func

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 import logging
 
-from telegram.ext import ApplicationBuilder
+from telegram.ext import ApplicationBuilder, JobQueue
 
 from src.config import settings
 from src.handlers.subscribers import command_handlers
@@ -10,17 +10,19 @@ from src.jobs import daily_check_job
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 
-def main() -> None:
-    app = ApplicationBuilder().token(settings.TELEGRAM_TOKEN).build()
-
-    app.add_handlers(command_handlers)
-
-    job_queue = app.job_queue
-    if not job_queue:
+def setup_jobs(job_queue: JobQueue | None):
+    if job_queue is None:
         logging.error("job queue is None")
         return
 
     job_queue.run_daily(daily_check_job, time=settings.SCHEDULE_UPDATE_TIME)
+
+
+def main() -> None:
+    app = ApplicationBuilder().token(settings.TELEGRAM_TOKEN).build()
+
+    app.add_handlers(command_handlers)
+    setup_jobs(app.job_queue)
 
     app.run_polling()
 
