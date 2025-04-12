42f8e6366c06c94b3474fdd7891b1486623fafad
github-main-user <kolya.skripn@mail.ru>
2025-04-12 18:23:12 +0300

    fix: handle format placeholders

--- src/handlers/special.py
+++ src/handlers/special.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
 
+from src.config import settings
 from src.utils import messages
 
 
@@ -8,9 +9,14 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if not update.effective_chat:
         return
 
+    message = messages.HELP.format(
+        update_time=settings.SCHEDULE_UPDATE_TIME.strftime("%H:%M"),
+        minutes_before=settings.MINUTES_BEFORE_LECTURE,
+    )
+
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=messages.HELP,
+        text=message,
     )
 
 
