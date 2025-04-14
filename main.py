b84070e987f8bfe1250525e7c034db9fcf905c85
github-main-user <kolya.skripn@mail.ru>
2025-04-14 19:02:05 +0300

    feat: start jobs for each lecture - minutes in settings

--- main.py
+++ main.py
@@ -3,21 +3,28 @@ import logging
 
 from telegram.ext import ApplicationBuilder, JobQueue
 
+from src import jobs
 from src.config import settings
 from src.handlers.schedule import schedule_handlers
 from src.handlers.special import special_handlers
 from src.handlers.subscribers import command_handlers
-from src.jobs import daily_check_job
+from src.utils.schedule_utils import subtract_minutes
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 
-def setup_jobs(job_queue: JobQueue | None):
+def setup_jobs(job_queue: JobQueue | None) -> None:
     if job_queue is None:
         logging.error("job queue is None")
         return
 
-    job_queue.run_daily(daily_check_job, time=settings.SCHEDULE_UPDATE_TIME)
+    job_queue.run_daily(jobs.daily_check_job, time=settings.SCHEDULE_UPDATE_TIME)
+
+    for time in settings.SCHEDULE_TIMES:
+        job_queue.run_daily(
+            jobs.notify_about_upcoming_lecture,
+            time=subtract_minutes(time, settings.MINUTES_BEFORE_LECTURE),
+        )
 
 
 def main() -> None:
