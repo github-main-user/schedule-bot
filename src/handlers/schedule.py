06df2f45866fc49cd16d994550d1ab8fe35684cb
github-main-user <kolya.skripn@mail.ru>
2025-04-14 13:43:34 +0300

    fix: use format_lecture util instead of manuall format

--- src/handlers/schedule.py
+++ src/handlers/schedule.py
@@ -6,7 +6,7 @@ from telegram.ext import CommandHandler, ContextTypes
 from src.config import settings
 from src.db import get_session
 from src.repositories.schedule_repository import ScheduleRepository
-from src.utils import messages
+from src.utils import messages, schedule_utils
 
 
 async def next(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -22,19 +22,9 @@ async def next(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         if not next_lecture:
             return
 
-        message = messages.LECTURE_VERBOSE_TEMPLATE.format(
-            date=next_lecture.date_time.strftime("%d %b (%a)"),
-            time=next_lecture.date_time.strftime("%H:%M"),
-            name=next_lecture.discipline.name,
-            cabinet=next_lecture.cabinet,
-            is_practice="Практика" if next_lecture.is_practice else "Лекция",
-            teacher=next_lecture.teacher.fullname,
-            age=next_lecture.teacher.age,
-        )
-
     await context.bot.send_message(
         chat_id=chat_id,
-        text=message,
+        text=schedule_utils.format_lecture_verbose(next_lecture),
         parse_mode="Markdown",
     )
 
