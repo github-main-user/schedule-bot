cc65533b6a471be007a92ecf114fc8d36cada97d
github-main-user <kolya.skripn@mail.ru>
2025-04-14 18:52:44 +0300

    feat: add util function for time subtraction

--- src/utils/schedule_utils.py
+++ src/utils/schedule_utils.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime, timedelta
+from datetime import date, datetime, time, timedelta
 
 import requests
 
@@ -21,6 +21,12 @@ def get_local_now() -> datetime:
 
 def get_tomorrow() -> date:
     return date.today() + timedelta(days=1)
+
+
+def subtract_minutes(original_time: time, minutes: int) -> time:
+    dummy_dt = datetime.combine(datetime.min, original_time)
+    new_datetime = dummy_dt - timedelta(minutes=minutes)
+    return new_datetime.time()
 
 
 def format_lecture(lecture: Lecture) -> str:
