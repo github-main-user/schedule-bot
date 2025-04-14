e5b3f2efe46b47f5b0ddda4fc88c68eaf9c9866d
github-main-user <kolya.skripn@mail.ru>
2025-04-14 13:41:16 +0300

    feat: extract lecture formatting to utils functions

--- src/utils/schedule_utils.py
+++ src/utils/schedule_utils.py
@@ -1,6 +1,9 @@
 import requests
 
 from src.config import settings
+from src.models.schedule import Lecture
+
+from . import messages
 
 
 def request_raw_schedule() -> list:
@@ -8,3 +11,23 @@ def request_raw_schedule() -> list:
     r.raise_for_status()
 
     return r.json().get("scheduleChanges", [])
+
+
+def format_lecture(lecture: Lecture) -> str:
+    return messages.LECTURE_BASE_TEMPLATE.format(
+        time=lecture.date_time,
+        name=lecture.name,
+        event_type="Практика" if lecture.is_practice else "Лекция",
+        cabinet=lecture.cabinet,
+    )
+
+
+def format_lecture_verbose(lecture: Lecture) -> str:
+    return messages.LECTURE_VERBOSE_TEMPLATE.format(
+        time=lecture.date_time,
+        name=lecture.name,
+        event_type="Практика" if lecture.is_practice else "Лекция",
+        cabinet=lecture.cabinet,
+        teacher=lecture.teacher.fullname,
+        age=lecture.teacher.age,
+    )
