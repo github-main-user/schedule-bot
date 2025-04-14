ee6caef9a054523e2768ffae29bf467d7b69e484
github-main-user <kolya.skripn@mail.ru>
2025-04-14 12:41:53 +0300

    fix: separate lecture templates

--- src/utils/messages.py
+++ src/utils/messages.py
@@ -7,8 +7,7 @@ Available commands:
 - /help - show this message
 - /start - start bot
 - /stop - stop bot
-- /next - get the nearest lecture
-"""
+- /next - get the nearest lecture"""
 
 SUBSCRIBED = "You are subscribed to receive info about upcoming lectures every day at {update_time}"
 UNSUBSCRIBED = "You are unsubscribed now"
@@ -18,8 +17,8 @@ NOT_SUBSCRIBED = "You are not subscribed"
 EMPTY_TOMORROW = "There are no lectures tomorrow"
 TOMORROW_N_LECTURES = "Tomorrow will be {n} lecture(s)"
 
-LECTURE_VERBOSE_TEMPLATE = """\
-*{date}*
-*{time}* {name} ({cabinet}) ({is_practice})
-{teacher} ({age} yeard old)
-"""
+DATE_TEMPLATE = "*{date:%d %b (%a)}*"
+
+LECTURE_BASE_TEMPLATE = "*{time:%H:%M}*: {name} ({type}) ({cabinet})"
+
+LECTURE_VERBOSE_TEMPLATE = LECTURE_BASE_TEMPLATE + "\n{teacher} ({age} yeard old)"
