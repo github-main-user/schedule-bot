39fd992a73649b16eef9929badd500e333c0d756
github-main-user <kolya.skripn@mail.ru>
2025-04-16 21:17:57 +0300

    feat: add logging to jobs

--- src/jobs.py
+++ src/jobs.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime, time
 
 from telegram.ext import ContextTypes
@@ -8,12 +9,16 @@ from src.repositories.subscriber_repository import SubscriberRepository
 from src.services.schedule import update_schedule
 from src.utils import global_utils, messages, schedule_utils
 
+logger = logging.getLogger(__name__)
+
 
 async def daily_schedule_update(context: ContextTypes.DEFAULT_TYPE) -> None:
     """
     Updates the local schedule.
     Notifies all subscribed users about tomorrow lectures.
     """
+    logger.info("Daily schedule update is being called")
+
     await update_schedule()
 
     async with await get_session() as session:
@@ -42,6 +47,8 @@ async def daily_schedule_update(context: ContextTypes.DEFAULT_TYPE) -> None:
                 text=(messages.DATE_TEMPLATE.format(date=tomorrow_date) + "\n" + formatted_lectures),
             )
 
+        logger.info("Subscribed user %s was notified about tomorrow lectures", subscriber.chat_id)
+
 
 async def notify_about_upcoming_lecture(context: ContextTypes.DEFAULT_TYPE) -> None:
     """
@@ -50,18 +57,20 @@ async def notify_about_upcoming_lecture(context: ContextTypes.DEFAULT_TYPE) -> N
     """
     job = context.job
     if not job:
+        logger.warning("Job is None")
         return
 
     lecture_time: time = job.data.get("original_time")  # type: ignore
-    exact_datetime = datetime.combine(date=datetime.today(), time=lecture_time)
+    exact_lecture_datetime = datetime.combine(date=datetime.today(), time=lecture_time)
 
     async with await get_session() as session:
         schedule_repo = ScheduleRepository(session)
         subscriber_repo = SubscriberRepository(session)
 
-        next_lecture = await schedule_repo.get_lecture_by_datetime(exact_datetime)
+        next_lecture = await schedule_repo.get_lecture_by_datetime(exact_lecture_datetime)
 
         if not next_lecture:
+            logger.info("There is no lecture at %s, leaving", exact_lecture_datetime)
             return
 
         subscribers = await subscriber_repo.get_all()
@@ -71,3 +80,4 @@ async def notify_about_upcoming_lecture(context: ContextTypes.DEFAULT_TYPE) -> N
             chat_id=subscriber.chat_id,
             text=schedule_utils.format_lecture_verbose(next_lecture),
         )
+        logger.info("Subscribed user %s was notified about upcoming lecture", subscriber.chat_id)
