7c41a64cd4f0d5c5c590c2a50b46d460a5f64b09
github-main-user <kolya.skripn@mail.ru>
2025-04-28 12:32:58 +0300

    feat: new local_today util function

--- src/utils/global_utils.py
+++ src/utils/global_utils.py
@@ -8,9 +8,21 @@ def get_local_now() -> datetime:
     return datetime.now(settings.TIMEZONE)
 
 
+def get_local_today() -> date:
+    """
+    Returns a `date` object which contains today's date.
+    Uses the timezone from settings.
+    """
+    now = datetime.now(settings.TIMEZONE)
+    return now.date()
+
+
 def get_tomorrow() -> date:
-    """Returns a `date` object containing tomorrow's date."""
-    return date.today() + timedelta(days=1)
+    """
+    Returns a `date` object which contains tomorrow's date.
+    Uses the timezone from settings.
+    """
+    return get_local_today() + timedelta(days=1)
 
 
 def subtract_minutes(original_time: time, minutes: int) -> time:
